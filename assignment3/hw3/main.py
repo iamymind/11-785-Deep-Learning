3ed9a903bd8969bfaf60feffb114e002dff056cb
danenigma <danielfekaduenigma@yahoo.com>
2018-03-18 01:48:03 +0300

    dropout

--- assignment3/hw3/main.py
+++ assignment3/hw3/main.py
@@ -168,7 +168,7 @@ def main(argv):
             for p in model.parameters():
                 p.data.add_(-args.lr, p.grad.data)
                 
-            optimizer.step()
+            #optimizer.step()
             #utils.adjust_learning_rate(optimizer, args, args.base_seq_len)
 
             epoch_loss += loss.data.sum()
