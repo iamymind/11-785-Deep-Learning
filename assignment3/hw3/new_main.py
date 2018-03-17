0fc47cb536f03cbe1d8c66f36bb4db17c0e4145c
danenigma <danielfekaduenigma@yahoo.com>
2018-03-18 01:19:33 +0300

    dropout

--- assignment3/hw3/new_main.py
+++ assignment3/hw3/new_main.py
@@ -165,6 +165,7 @@ def main(argv):
             optimizer.zero_grad()
 
             X, y, seq_len = next(train_data_loader)
+            print('X: ', X.shape, 'y: ', y.shape)
             #hidden = repackage_hidden(hidden)
             #out, hidden = model(X, hidden)
             out = model(X)
